57d2bb045248c70611545c8c41e2d4f435ae418f
0Magenta0 <0_magenta_0@mail.ru>
2023-01-17 22:15:55 +0300

    Upd: builder.sh add license comment

--- build/builder.sh
+++ build/builder.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#---------------==================================================---------------#
+#  build/builder.sh                                                              #
+#  This file is a part of the GetIP distribution released under GNU GPLv3        #
+#  Copyright (C) 2021-2023  0Magenta0                                            #
+#  https://github.com/0Magenta0/GetIP                                            #
+#---------------==================================================---------------#
 
 # Clean (Remove everything except himself)
 if [[ $(echo "$1" | grep -Eq "^.*C.*$"; echo $?) -eq 0 ]]; then
